feb080bf7ae85d90ae6ebfaf466cbcf12bbbae00
qatip <michael2697@hotmail.com>
2025-03-24 12:48:42 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.26.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "qatipv3"
+}
+
+resource "google_compute_network" "lab_vpc" {
+  name                    = "lab-vpc"
+  auto_create_subnetworks = true
+}
+
+terraform {
+ backend "gcs" {
+   bucket  = "terraform-state-2697"  
+   prefix  = "terraform/state"
+ }
+}
