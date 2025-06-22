266a6877f496866496cc001a35686bf6377bc22b
qatip <michael2697@hotmail.com>
2025-06-22 20:12:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket         = "{your state bucket}"   
+    bucket         = "https://github.com/qatip/jenkins.git"   
     key            = "terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
