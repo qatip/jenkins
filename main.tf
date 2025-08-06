27b037da8d15e5b8298dcf58d7367a0cc276d88c
qatip <michael2697@hotmail.com>
2025-08-06 23:20:35 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "jenkinsstatemichael2697"
+    storage_account_name = "jenkinsstatemichael123"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
