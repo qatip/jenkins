403ee027e5713787a1676191c8d0c369b7c7ffb0
qatip <michael2697@hotmail.com>
2025-09-06 22:34:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "https://github.com/qatip/jenkins.git"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstatemichael"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
