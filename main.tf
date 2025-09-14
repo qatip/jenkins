5d9b8630081813bda77591afc7b70f2df020760c
qatip <michael2697@hotmail.com>
2025-09-14 16:43:12 +0100

    main update

--- main.tf
+++ main.tf
@@ -152,7 +152,7 @@ resource "azurerm_linux_virtual_machine" "vm1" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = var.ssh_public_key  # ← no file()
+    public_key = var.ssh_public_key
   }
 
   source_image_reference {
