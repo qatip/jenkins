18b8f9ff6216e4f3c9cb41d4b7c1908a2e04801d
qatip <michael2697@hotmail.com>
2025-09-16 18:35:03 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "google" {
+  project = "qwiklabs-gcp-04-6c6a38f70464"  # Replace with your GCP project ID
+  region  = "us-west1"            # Replace with your desired GCP region
+}
+
+terraform {
+  backend "gcs" {
+    bucket  = "jenkins-state-michael"  # Replace with the name of your GCS bucket
+    prefix  = "terraform/state"    # The folder path in the bucket
+  }
+}
+
+resource "google_compute_network" "example_vpc" {
+  name                    = "example-vpc"
+  auto_create_subnetworks = true  # Automatically creates subnets for all regions
+}
+
